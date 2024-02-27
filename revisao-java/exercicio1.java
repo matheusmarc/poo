01886f2af8cde8442339ba3ca79aa1010e0e835f
Matheus Marcelino da Silva <matheus.ms22@aluno.ifsc.edu.br>
2024-02-27 19:31:46 -0300

    revisao-java/

--- revisao-java/exercicio1.java
+++ revisao-java/exercicio1.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;
+
 public class exercicio1{
-    
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Nome: ");
+        String nome = teclado.nextLine();
+
+        System.out.println("Ano de nascimento: ");
+        int anoNasc = teclado.nextInt();
+        
+        System.out.println("Ano atual: ");
+        int anoAtual = teclado.nextInt();
+
+        System.out.println(nome+", "+"possui "+(anoAtual-anoNasc)+" anos");
+    }
 }
