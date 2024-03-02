5a0dc23f9a7b8766033d1476cbd789cd0097537f
Matheus Marcelino da Silva <matheus.ms22@grad.ufsc.br>
2024-03-01 21:25:55 -0300

    aula05

--- aula05/app/src/main/java/org/example/App.java
+++ aula05/app/src/main/java/org/example/App.java
@@ -3,12 +3,17 @@
  */
 package org.example;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import java.util.Scanner;
 
+public class App {
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+
+        Scanner linha = new Scanner(System.in);
+
+        while(linha.hasNext()){
+            String aux = linha.nextLine();
+
+            System.out.println(aux.toUpperCase());
+        }
     }
 }
